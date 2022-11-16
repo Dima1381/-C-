8e865edd9769ef5a7e00a4af1254fc06fe9d7f48
Dima <114566098+Dima1381@users.noreply.github.com>
2022-11-16 18:42:11 +0200

    Add files via upload

---
+++ Дії над масивом/_1_.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+
+using namespace std;
+
+char* voidNumberAsBinary(char* _result, unsigned int _number)
+{
+	int number = _number;
+	int i = 0;
+
+	while (number > 0)
+	{
+		_result[i] = (number % 2) + '0';
+		number /= 2;
+		i++;
+	}
+
+	for (size_t j = 0; j < i / 2; j++)
+	{
+		swap(_result[j], _result[i - j - 1]);
+	}
+
+	_result[i] = '\0';
+	return _result;
+}
+
+int main()
+{
+	srand(time(NULL));
+
+	unsigned int random = 0;
+	char* result = new char[11]{};
+
+	for (size_t i = 0; i < 10; i++)
+	{
+		random = rand() % 513;
+		cout << "[ " << i << " ]  Number ( " << random << " ) " << "  Binary = " << voidNumberAsBinary(result, random) << endl;
+	}
+
+	delete[] result;
+	return 0;
+}
---
+++ Дії над масивом/_4_.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void GenerateRandomArray(int* _result, unsigned int _maxValue)
+{
+    for (int i = 0; i < _maxValue; i++)
+    {
+        _result[i] = 5 + rand() % (15 - 5 + 1);
+        cout << _result[i] << " ";
+    }
+}
+
+int GetMissingValue(const int* _values, unsigned int _count)
+{
+    int sum1 = 0, sum2 = 0;
+
+    for (int i = 0; i < _count; i++)
+    {
+        sum1 += _values[i];
+    }
+    for (int i = 0; i < _count - 1; i++)
+    {
+        sum2 += _values[i];
+    }
+
+    return sum1 - sum2;
+}
+
+int main()
+{
+    srand(time(NULL));
+    cout << "Enter size array : ";
+    int N = 0;
+    cin >> N;
+
+    int* arr = new int[N];
+
+    GenerateRandomArray(arr, N);
+    cout << endl;
+    cout << "Last elements : " << GetMissingValue(arr, N);
+
+
+    delete[] arr;
+}
+
