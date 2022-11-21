4884a658a813fc3d5b1a8d5cb2b111818e5d4edb
Dima <114566098+Dima1381@users.noreply.github.com>
2022-11-21 13:43:54 +0200

    Add files via upload

---
+++ Longest Substring Without Repeating Characters/Longest Substring Without Repeating Characters.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+int main()
+{
+	char str[] = "pwwkew";
+	int res = 0;
+	int start = -1;
+
+	vector<int> dect(256, -1);
+
+	for (int i = 0; i < strlen(str); i++)
+	{
+		if (dect[str[i]] > start)
+		{
+			start = dect[str[i]];
+		}
+		dect[str[i]] = i;
+		res = max(res, i - start);
+	}
+
+	cout << res << endl;
+}
+
