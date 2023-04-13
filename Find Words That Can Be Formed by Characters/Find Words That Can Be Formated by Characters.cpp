292de7417514819106d2e5c994a80fbf9a1fd4ea
Dima1381 <dimalyzun100@gmail.com>
2023-04-13 13:01:38 +0300

    add files

---
+++ Find Words That Can Be Formed by Characters/Find Words That Can Be Formated by Characters.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int countCharacters(vector<string>& words, string chars) 
+{
+	int answer = 0;
+
+	for (int i = 0; i < words.size(); i++)
+	{
+		vector<int> Ischar(26, 0);
+
+		for (int j = 0; j < chars.length(); j++)
+			Ischar[chars[j] - 'a']++;
+
+		bool cheak = true;
+		for (int j = 0; j < words[i].size(); j++)
+		{
+			if (Ischar[(words[i][j]) - 'a'] == 0)
+			{
+				cheak = false;
+				break;
+			}
+			Ischar[(words[i][j]) - 'a']--;
+		}
+
+		if (cheak)
+			answer += words[i].size();
+	}
+
+	return answer;
+}
+
+int main()
+{
+	vector<string> words = { "cat", "bt", "hat", "tree" };
+	string chars = "atach";
+
+	int answer = countCharacters(words, chars);
+
+	return 0;
+}
