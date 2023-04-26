d31684660faf42e263fcfd784abcf8c868931a0f
Dima1381 <dimalyzun100@gmail.com>
2023-04-26 17:46:31 +0300

    Rank Transform of an Array

---
+++ Rank Transform of an Array/Rank Transform of an Array.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+vector<int> arrayRankTransform(vector<int>& arr) 
+{
+	map<int, int> mp;
+
+	for (auto& num : arr)
+	{
+		mp[num] = 0;
+	}
+	int rank = 1;
+
+	for (auto& num : mp)
+	{
+		num.second = rank++;
+	}
+
+	for(int i = 0; i < arr.size(); i++)
+	{
+		arr[i] = mp[arr[i]];
+	}
+	return arr;
+}
+
+int main()
+{
+	vector<int> arr = { 40,40, 10, 20, 30 };
+
+    vector<int> answer = arrayRankTransform(arr);
+
+	return 0;
+}
